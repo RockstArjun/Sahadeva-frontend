1182f164b37ff3686122980bf3b96156408abb32
Atharva <atharvajadhav25@gmail.com>
2023-09-14 01:29:34 +0530

    Added landing page

---
+++ Landing page/src/components/HeroSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>SAHADEVA</h1>
+      <p>Your Partner to all the Technical queries!</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          Ask Me!
+        </Button>
+        {/* <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button> */}
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
